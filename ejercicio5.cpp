0ae8ab62a0eac6cc5ab9cebfcec34a5d3186530d
FrancoBolanos <franco.bolanos@utec.edu.pe>
2019-08-29 21:05:39 -0500

    ejercicio5 resuelto

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -24,5 +24,20 @@ using namespace std;
 
 int main() {
     float a, b, c, delta, x1, x2;
+    cin>>a>>b>>c;
+    delta=(b*b-4*a*c);
 
+
+    if (a==0 and b==0) {
+        cout<<"No tiene solucion\n";
+    } else if (a==0) {
+        cout<<"x = "<<-c/b<<"\n";
+    } else if (delta<0){
+        cout<<"No tiene solucion\n";
+    } else {
+        x1=(-b+(sqrt(delta)))/(2*a);
+        x2=(-b-(sqrt(delta)))/(2*a);
+        cout<<"x1 = "<<x1<<", x2 = "<<x2<<"\n";
+    }
+    return 0;
 }
